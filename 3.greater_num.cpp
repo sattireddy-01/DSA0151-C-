c55d098ed87a1ce5627292d66e1180f3729084e4
sattireddy-01 <132328195+sattireddy-01@users.noreply.github.com>
2024-05-15 09:59:18 +0530

    Add files via upload

---
+++ 3.greater_num.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+int main(){
+	int n1,n2;
+	std::cout << "enter num1";
+	std::cin >> n1;
+	std::cout << "enter num2";
+	std::cin >> n2;
+	if(n1>n2){
+		std::cout<<n1<< "is greater";
+	}
+	else{
+		std::cout<<n2<< "is greater";
+	}
+	
+}
