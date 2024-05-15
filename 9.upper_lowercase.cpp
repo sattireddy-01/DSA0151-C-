ba5d1052df3e6af2d48e0a9fd38227b460a4c4eb
sattireddy-01 <132328195+sattireddy-01@users.noreply.github.com>
2024-05-15 10:01:58 +0530

    Add files via upload

---
+++ 9.upper_lowercase.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+int main() {
+    std::string str;
+    std::cout << "Enter a string: ";
+    std::getline(std::cin, str);
+    std::transform(str.begin(), str.end(), str.begin(), ::toupper);
+    std::cout << "Transformed string: " << str << std::endl;
+    return 0;
+}
